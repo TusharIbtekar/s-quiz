9143d0c0666a88c4cdbc998e8a6c6423f4093c68
TusharIbtekar <ibtekar8@gmail.com>
2024-01-21 00:30:50 +0600

    feat: complete result view

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import SignIn from "./pages/SignIn";
 import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
+        <Route path="/result/:id" element={<Result />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/admin" element={<Admin />} />
---
+++ src/pages/Result/index.tsx
@@ -0,0 +1,76 @@
+import { useResultStore } from "@/stores/result";
+import { useParams } from "react-router-dom";
+import { Option } from "@/stores/quiz";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Result() {
+  const results = useResultStore((state) => state.results);
+  const id = useParams().id;
+  const userResults = results["1"];
+  const userResult = userResults.find((result) => result.quizId === id);
+  const score = userResult?.answers.reduce((acc, answer) => {
+    if (answer.currentAnswer === answer.correctAnswer) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+  return (
+    <>
+      <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Your Result
+      </h1>
+      <p className="scroll-m-20 text-xl tracking-tight mt-5">
+        You scored {score} out of {userResult?.total}
+      </p>
+      {userResult?.answers.map((answer) => {
+        return (
+          <>
+            <Card key={answer.question.id} className="my-5">
+              <CardHeader>
+                <CardTitle>{answer.question.title}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-green-800">
+                  Correct Answer:{" "}
+                  {
+                    (
+                      answer.question.options.find(
+                        (o) => o.id === answer.correctAnswer
+                      ) as Option
+                    ).label
+                  }
+                </p>
+                {answer.correctAnswer !== answer.currentAnswer && (
+                  <p className="text-red-500">
+                    Your Answer:{" "}
+                    {
+                      (
+                        answer.question.options.find(
+                          (o) => o.id === answer.currentAnswer
+                        ) as Option
+                      ).label
+                    }
+                  </p>
+                )}
+                {answer.previousAnswers.map((previousAnswer, index) => {
+                  return (
+                    <p className="text-blue-500">
+                      Previous Answer {index + 1} :{" "}
+                      {
+                        (
+                          answer.question.options.find(
+                            (o) => o.id === previousAnswer
+                          ) as Option
+                        ).label
+                      }
+                    </p>
+                  );
+                })}
+              </CardContent>
+            </Card>
+          </>
+        );
+      })}
+    </>
+  );
+}
