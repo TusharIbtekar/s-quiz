e5ce2cc542b98b2200e34ee7f7a610e9c316cfdb
TusharIbtekar <ibtekar8@gmail.com>
2024-01-21 23:19:09 +0600

    chore: fix build error

--- src/pages/EditQuiz/index.tsx
+++ src/pages/EditQuiz/index.tsx
@@ -1,40 +1,12 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { quizSchema } from "@/lib/schemas";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
-import { nanoid } from "nanoid";
 import { Quiz, useQuizStore } from "@/stores/quiz";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import QuizForm from "@/components/quiz/QuizForm";
 
 type formData = z.infer<typeof quizSchema>;
 
 const EditQuiz = () => {
-  const navigate = useNavigate();
   const quizId = useParams().id;
   const quiz = useQuizStore(
     (state) => state.quizes.find((q) => q.id === quizId) as Quiz
